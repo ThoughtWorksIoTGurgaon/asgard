3473e40e60feb30888632aad8a18494b64c98f95
Sumedha Verma <sumedhaverma6@gmail.com>
2015-09-04 18:49:09 +0530

    Logging in console when device is swtiched on or off

--- client/app/scripts/controllers/devices.js
+++ client/app/scripts/controllers/devices.js
@@ -2,46 +2,53 @@
 
 angular.module('cloudStoreClient')
   .controller('DevicesCTRL',function DeviceCTRL($scope) {
-	  
-		$scope.sortableOptions = {
-			disabled: false
-		};
-		$scope.status=false;
-		$scope.oneAtATime = true;
-		$scope.devices = [
-		    {
-		        "Title": "Master Bedroom",
-		        "Description": "Controls the master bedroom",
-		        "Id": "ABCD12345",
-		        "Status": "UP",
-		        "Temperature": "34",
-		        "Switches": [
-		            {
-		                "Label": "Fan",
-		                "State": true
-		            },
-		            {
-		                "Label": "AC",
-		                "State": true
-		            }
-		        ]
-		    },
-		   {
-		        "Title": "Master Kitchen",
-		        "Description": "Controls the chef",
-		        "Id": "ABCD1233",
-		        "Status": "UP",
-		        "Temperature": "34",
-		        "Switches": [
-		            {
-		                "Label": "Fan",
-		                "State": false
-		            },
-		            {
-		                "Label": "Fridge",
-		                "State": true
-		            }
-		        ]
-		    }
-			];		
-		  });
+    
+    $scope.sortableOptions = {
+      disabled: false
+    };
+    $scope.status=false;
+    $scope.oneAtATime = true;
+    $scope.devices = [
+      {
+        "title": "Master Bedroom",
+        "description": "Controls the master bedroom",
+        "id": "ABCD12345",
+        "status": "UP",
+        "temperature": "34",
+        "switches": [
+            {
+                "label": "Fan",
+                "state": true,
+                "id" : "1234"
+            },
+            {
+                "label": "AC",
+                "state": true,
+                "id": "abc"
+            }
+        ]
+      },
+     {
+        "title": "Master Kitchen",
+        "description": "Controls the chef",
+        "id": "ABCD1233",
+        "status": "UP",
+        "temperature": "34",
+        "switches": [
+            {
+                "label": "Fan",
+                "state": false,
+                "id": "xyz"
+            },
+            {
+                "label": "Fridge",
+                "state": true,
+                "id": "def"
+            }
+        ]
+      }
+    ]; 
+    $scope.changeState = function() {
+      console.log("Hello!!!!!");
+    };   
+});
