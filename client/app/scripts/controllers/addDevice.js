f63e878220b27515492b5592b223877ea0bf2f49
Sumedha Verma <sumedhaverma6@gmail.com>
2015-09-18 16:02:39 +0530

    Adding form fields to the modal. Passing the form data when pressing submit on modal

--- client/app/scripts/controllers/addDevice.js
+++ client/app/scripts/controllers/addDevice.js
@@ -2,38 +2,36 @@
 
 angular.module('cloudStoreClient')
     .controller('AddDeviceCtrl', function ($scope,$http,$modal) {
-	$scope.hello = function() {
-	    $scope.serviceAddresses = ['1234', 'ABCD', 'WXYZ'];
-	    $scope.animationsEnabled = true;
-	    $scope.open = function(selected_service){
+  $scope.hello = function() {
+      $scope.serviceAddresses = ['1234', 'ABCD', 'WXYZ'];
+      $scope.animationsEnabled = true;
+      $scope.open = function(selected_service){
 
-		var modalInstance = $modal.open({
-		    animation: $scope.animationsEnabled,
-		    templateUrl: 'myModalContent.html',
-		    controller: 'ModalInstanceCtrl',
-		    selected_service: selected_service,
-		    resolve: {
-			selected_service_address: function () {
-			    return selected_service;
-			}
-		    }
-		});
-		
-		console.log($modal);
-	    }
-	};   
-    });
+    var modalInstance = $modal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        selected_service: selected_service,
+        resolve: {
+      selected_service_address: function () {
+          return selected_service;
+      }
+        }
+    });    
+    }
+  };   
+});
 
 
 angular.module('cloudStoreClient').controller('ModalInstanceCtrl', function ($scope, $modalInstance, selected_service_address) {
+  $scope.selected_service_address = selected_service_address;
 
-    $scope.selected_service_address = selected_service_address;
+  $scope.submit = function(device) {
+    $modalInstance.close($scope.selected_service_address);
+    console.log(device);
+  };
 
-    $scope.ok = function () {
-	$modalInstance.close($scope.selected_service_address);
-    };
-
-    $scope.cancel = function () {
-	$modalInstance.dismiss('cancel');
-    };
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 });
